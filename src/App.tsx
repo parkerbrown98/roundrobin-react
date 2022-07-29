f3b94b7d438affeb9ab2bcac0230ee9bc2587469
Parker Brown <brownpt98@gmail.com>
2022-07-29 01:27:41 -0500

    Add react-hot-toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import Container from './components/Container';
 import PeopleManager from './components/PeopleManager';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
     return (
-        <Container>
-            <h1 className="mt-16 text-center text-5xl sm:text-6xl font-bold">Pair Generator</h1>
-            <h4 className="text-center text-xl sm:text-2xl text-stone-400 font-semibold">(with rounds)</h4>
-            <PeopleManager />
-        </Container>
+        <>
+            <Toaster position='top-center' />
+            <Container>
+                <h1 className="mt-16 text-center text-5xl sm:text-6xl font-bold">Pair Generator</h1>
+                <h4 className="text-center text-xl sm:text-2xl text-stone-400 font-semibold">(with rounds)</h4>
+                <PeopleManager />
+            </Container>
+        </>
     )
 }
 
